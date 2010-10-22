79541a3c3c105e9b2d9fecaa1061423c3907b15c
Daisuke (yet another) Maki <maki.daisuke@gmail.com>
2010-10-23 03:26:21 +0900

    initial commit

---
+++ lib/AnyEvent/JSONRPC/Bidirectional.pm
@@ -0,0 +1,32 @@
+package AnyEvent::JSONRPC::Bidirectional;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+AnyEvent::JSONRPC::Bidirectional -
+
+=head1 SYNOPSIS
+
+  use AnyEvent::JSONRPC::Bidirectional;
+
+=head1 DESCRIPTION
+
+AnyEvent::JSONRPC::Bidirectional is
+
+=head1 AUTHOR
+
+Daisuke (yet another) Maki E<lt>yanother@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
