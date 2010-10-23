60ce5151e2f17eea98e7534325710032338160f6
Daisuke (yet another) Maki <maki.daisuke@gmail.com>
2010-10-23 19:40:20 +0900

    initial import

--- lib/AnyEvent/JSONRPC/Bidirectional.pm
+++ lib/AnyEvent/JSONRPC/Bidirectional.pm
@@ -3,6 +3,154 @@ use strict;
 use warnings;
 our $VERSION = '0.01';
 
+package AnyEvent::JSONRPC::Bidirectional;
+
+use AnyEvent::Handle;
+use Carp;
+use Try::Tiny;
+use UNIVERSAL qw/isa/;
+
+
+sub new {
+    my ($class, %args) = @_;
+    my $callbacks   = delete $args{callbacks};
+    my $on_error_cb = delete $args{on_error} || sub{};
+    my $self;
+    my $on_error = sub{
+        my ($fatal, $msg) = @_;
+        $self->destroy  if $fatal;
+        $on_error_cb->($self, $fatal, $msg);
+    };
+    $args{on_error} = sub{
+        my ($hdl, $fatal, $msg) = @_;
+        $on_error->($fatal, $msg);
+    };
+    my $on_eof_cb = delete $args{on_eof} || sub{};
+    $args{on_eof} = sub{
+        $on_eof_cb->(@_);
+        $on_error->(1, "connection closed by peer");
+    };
+    $args{on_read} = sub {
+        shift->unshift_read(json => sub{ $self->_dispatch($_[1]) });
+    };
+    $self = bless {
+        _hdl       => AnyEvent::Handle->new(%args),
+        _on_error  => $on_error,
+        _msgid     => 0,
+        _callbacks => isa($callbacks, "HASH") ? $callbacks : {},
+        _res_cvs   => {},
+    }, $class;
+    $self;
+}
+
+sub destroy {
+    my $self = shift;
+    delete($self->{_hdl})->destroy  if $self->{_hdl};
+    delete($self->{_on_error});
+    delete($self->{_callbacls});
+    delete($self->{_res_cvs});
+}
+
+sub call {
+    my ($self, $method, $args, $cb) = @_;
+    return  unless $self->{_hdl};
+    croak "args must be an arrayref"    unless isa($args, 'ARRAY');
+    croak "callback must be a coderef"  unless isa($cb, 'CODE');
+    my $id = $self->{_msgid}++;
+    my $cv = AE::cv;
+    $cv->cb($cb);
+    $self->{_res_cvs}{$id} = $cv;
+    $self->{_hdl}->push_write(json => {id => $id, method => "$method", params => $args});
+}
+
+sub notify {
+    my ($self, $method, $args) = @_;
+    return  unless $self->{_hdl};
+    croak "args must be an arrayref"  unless isa($args, 'ARRAY');
+    $self->{_hdl}->push_write(json => {id => undef, method => "$method", params => $args});
+}
+
+sub _dispatch {
+    my ($self, $json) = @_;
+    if ( exists $json->{method} ) {
+        if ( defined $json->{id} ) {
+            $self->_dispatch_request($json);
+        } else {
+            $self->_dispatch_notification($json);
+        }
+    } else {
+        $self->_dispatch_response($json);
+    }
+}
+
+sub _dispatch_request {
+    my ($self, $json) = @_;
+    my ($id, $method, $args) = @{$json}{qw/id method params/};
+    my $sub = $self->{_callbacks}{$method}  or return $self->_return_error($id, "unknown method: $method");
+    isa($args, 'ARRAY')                     or return $self->_return_error($id, "invalid format message");
+    try {
+        my $r = $sub->(@$args);
+        if ( isa($r, 'CODE') ) {
+            my $cv = AE::cv;
+            $cv->cb(sub{
+                try {
+                    $r = $cv->recv;
+                    $self->_return_result($id, $r);
+                } catch {
+                    $self->_return_error($id, $_);
+                } finally {
+                    undef $cv;
+                }
+            });
+            $r->($cv);
+        } else {
+            $self->_return_result($id, $r);
+        }
+    } catch {
+        $self->_return_error($id, $_);
+    }
+}
+
+sub _return_result {
+    my ($self, $id, $val) = @_;
+    return  unless $self->{_hdl};
+    $self->{_hdl}->push_write(json => {id => $id, error => undef, result => $val});
+}
+
+sub _return_error {
+    my ($self, $id, $err) = @_;
+    return  unless $self->{_hdl};
+    $self->{_hdl}->push_write(json => {id => $id, error => $err, result => undef});
+}
+
+use Data::Dumper;
+sub _dispatch_response {
+    my ($self, $json) = @_;
+    my ($id, $error, $result) = @{$json}{qw/id error result/};
+    my $cv = delete($self->{_res_cvs}{$id});
+    return $self->{_on_error}->(0, "unknown response msgid: $id")  unless $cv;
+    if ( defined $error ) {
+        $cv->croak($error);
+    } else {
+        $cv->send($result);
+    }
+}
+
+sub _dispatch_notification {
+    my ($self, $json) = @_;
+    my ($method, $args) = @{$json}{qw/method params/};
+    my $sub = $self->{_callbacks}{$method}  or return $self->{_on_error}->(0, "unknown method: $method");
+    isa($args, 'ARRAY')                     or return $self->{_on_error}->(0, "invalid format message");
+    my $r = $sub->(@$args);
+    if ( isa($r, 'CODE') ) {
+        my $cv = AE::cv;
+        $cv->cb(sub{
+            undef $cv;
+        });
+        $r->($cv);
+    }
+}
+
 1;
 __END__
 
